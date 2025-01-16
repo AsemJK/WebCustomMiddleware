b3f41956d85ce37aa589d09c9f3c4d38480e97ff
AsemJK <asemalsaiyadi@gmail.com>
2025-01-16 20:29:50 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebCustomMiddleware.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Services/CustomMiddle.cs
@@ -0,0 +1,24 @@
+﻿namespace WebCustomMiddleware.Services
+{
+    public class CustomMiddle
+    {
+        private readonly RequestDelegate _next;
+
+        public CustomMiddle(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task Invoke(HttpContext context)
+        {
+            // Do something before the next middleware
+            if (!context.Request.Headers.ContainsKey("Authorizatione"))
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("Not Authorized");
+                return;
+            }
+            await _next(context);
+            // Do something after the next middleware
+        }
+    }
+}
